d4a253afedb12ee903e12d6c73bc90374811dc7a
Mohsen Parsa <mohsenp249@gmail.com>
2025-04-05 23:08:28 -0900

    create new product in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,54 +1,118 @@
+"use client";
 import Container from "@/components/Container";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+
+interface Iproduct {
+  imageUrl: string;
+  title: string;
+  subTitle: string;
+  rating: string;
+  price: string;
+}
 
 function Dashboard() {
+  const [product, setProduct] = useState<Iproduct>({
+    imageUrl: "",
+    title: "",
+    subTitle: "",
+    rating: "",
+    price: "",
+  });
+
+  const handleSetProduct = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const handleCreateNewProduct = () => {
+    axios({
+      method: "POST",
+      url: "https://dummyjson.com/products/",
+      data: {
+        imageUrl: product.imageUrl,
+        title: product.title,
+        subTitle: product.subTitle,
+        rating: product.rating,
+        price: product.price,
+      },
+    });
+  };
+
   return (
     <div>
       <Container>
-        <h3 className="text-2xl font-bold text-slate-600 mt-4">Dashboard of store </h3>
+        <h3 className="text-2xl font-bold text-slate-600 mt-4">
+          Dashboard of store{" "}
+        </h3>
         <div className="bg-gray-100 px-8 py-6 my-5 space-y-4">
           <div className="space-y-2">
-            <label htmlFor="image" className="inline-block">Image url</label>
+            <label htmlFor="image" className="inline-block">
+              Image url
+            </label>
             <input
+              onChange={(e) => handleSetProduct(e)}
               type="text"
+              name="imageUrl"
               placeholder="url"
               className="w-full rounded py-1 px-3 bg-white placeholder:text-gray-300 outline-0 border-0"
             />
           </div>
           <div className="space-y-2">
-            <label htmlFor="title" className="inline-block">Title</label>
+            <label htmlFor="title" className="inline-block">
+              Title
+            </label>
             <input
+              onChange={(e) => handleSetProduct(e)}
+              name="title"
               type="text"
               placeholder="title"
               className="w-full rounded py-1 px-3 bg-white placeholder:text-gray-300 outline-0 border-0"
             />
           </div>
           <div className="space-y-2">
-            <label htmlFor="subTitle" className="inline-block">Sub Title</label>
+            <label htmlFor="subTitle" className="inline-block">
+              Sub Title
+            </label>
             <input
+              onChange={(e) => handleSetProduct(e)}
+              name="subTitle"
               type="text"
               placeholder="subTitle"
               className="w-full rounded py-1 px-3 bg-white placeholder:text-gray-300 outline-0 border-0"
             />
           </div>
           <div className="space-y-2">
-            <label htmlFor="rating" className="inline-block">Rating</label>
+            <label htmlFor="rating" className="inline-block">
+              Rating
+            </label>
             <input
+              onChange={(e) => handleSetProduct(e)}
+              name="rating"
               type="text"
               placeholder="rating"
               className="w-full rounded py-1 px-3 bg-white placeholder:text-gray-300 outline-0 border-0"
             />
           </div>
           <div className="space-y-2">
-            <label htmlFor="price" className="inline-block">Price</label>
+            <label htmlFor="price" className="inline-block">
+              Price
+            </label>
             <input
+              onChange={(e) => handleSetProduct(e)}
+              name="price"
               type="text"
-              placeholder="rating"
+              placeholder="price"
               className="w-full rounded py-1 px-3 bg-white placeholder:text-gray-300 outline-0 border-0"
             />
           </div>
 
-          <button className="text-white bg-green-500 rounded-lg px-8 py-2 cursor-pointer active:scale-98 transition duration-75 shadow-md active:shadow-none">Submit</button>
+          <button
+            onClick={handleCreateNewProduct}
+            className="text-white bg-green-500 rounded-lg px-8 py-2 cursor-pointer active:scale-98 transition duration-75 shadow-md active:shadow-none"
+          >
+            Submit
+          </button>
         </div>
       </Container>
     </div>
